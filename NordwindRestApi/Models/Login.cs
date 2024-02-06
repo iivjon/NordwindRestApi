76519dff988de4515719cb0f1988bd892c1016d8
Joni Iivonen <iivonen.joni@gmail.com>
2024-02-07 00:08:21 +0200

    Add project files.

---
+++ NordwindRestApi/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NordwindRestApi.Models
+{
+    public partial class Login
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string UserPassword { get; set; } = null!;
+    }
+}
