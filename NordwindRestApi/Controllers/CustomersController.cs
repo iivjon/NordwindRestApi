208a3512814024781c891da163239055f8d04d1b
Joni Iivonen <iivonen.joni@gmail.com>
2024-02-08 10:52:46 +0200

    Lisäystoiminto

--- NordwindRestApi/Controllers/CustomersController.cs
+++ NordwindRestApi/Controllers/CustomersController.cs
@@ -14,8 +14,60 @@ namespace NordwindRestApi.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe. LUe lisää" + ex.InnerException);
+            }
         }
+
+        //Hakee yhden asiakkaan pääavaimella
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                //Customer asiakas = db.Customers.Find(); //Tämä toimii samalla tavalla, mut on vahvasti tyypitetty
+                var asiakas = db.Customers.Find(id);
+
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    //return BadRequest("Asiakasta id:llä" + id + " ei löydy.");
+                    return NotFound($"Asiakasta id:llä {id}  ei löydy."); //string interpolation
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe haettaessa avain asiakasta" + ex);
+            }
+        }
+
+        //Uuden lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust) 
+        {
+            try 
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi asiakas {cust.CompanyName} from {cust.City} ");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää:" + e.InnerException);
+            }
+        
+        
+        }
+
     }
 }
